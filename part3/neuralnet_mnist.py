a67b64e4e64c0c5031f42ea71a7f181d78d1771d
Hyeonjun-Heo <camaro922@naver.com>
2023-08-10 22:49:03 +0900

    2023-08-10

---
+++ part3/neuralnet_mnist.py
@@ -0,0 +1,10 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+    load_mnist(normalize = True, flatten = True, one_hot_label = False)
