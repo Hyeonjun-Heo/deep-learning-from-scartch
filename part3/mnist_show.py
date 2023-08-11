3bbdfefd3c590fa1b6f9758e46e56cb9c29d5df6
camaro922 <camaro922@naver.com>
2023-08-11 22:11:35 +0900

    2023-08-11

--- part3/mnist_show.py
+++ part3/mnist_show.py
@@ -1,10 +1,26 @@
 import sys, os
 sys.path.append(os.pardir) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
 from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
 # 처음 한 버은 몇 분 정도 걸립니다.
 (x_train, t_train), (x_test, t_test) = \
   load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(label) # 5
+
+print(img.shape) # (784,)
+img = img.reshape(28, 28)
+print(img.shape) # (28,28)
+
+img_show(img)
 
 # 각 데이터의 형상 출력
 print(x_train.shape) # (60000, 784)
